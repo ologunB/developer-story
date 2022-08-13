0ac941913d68dd993d3ee671f58fb0b8883f68ee
Temitope Ologunbaba <temitope@glade.ng>
2022-08-13 12:35:52 +0100

    Remove komodo

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,3 +3,22 @@ export const avatarImageSize = 64;
 export const itemImageSize = 48;
 export const iconSize = 18;
 export const dateFormat = "MMM YYYY";
+
+
+/**{
+    "name": "Mobile Engineer",
+    "company": "Komodo Inc",
+    "website": "https://komodoplatform.com/en/",
+    "description": "Part of a global team creating dex for crypto and maintaining the alpha version of the software using mainly Dart, Java and using Agile/SCRUM as methodology on Github.",
+    "start": "2022-04-01T00:00:00Z",
+    "end": "2022-08-31T00:00:00Z",
+    "technologies": [
+        "mm2-libs", "crypto",
+        "android",
+        "ios",
+        "flutter",
+        "dart",
+        "firebase"
+    ],
+    "icon": "kmd"
+},**/
