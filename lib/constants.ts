08ed5b1449bea69de9135105a79ea67e488c9ffc
Temitope Ologunbaba <temitope@glade.ng>
2023-05-15 11:42:54 +0100

    add kmd

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,21 +3,3 @@ export const avatarImageSize = 64;
 export const itemImageSize = 48;
 export const iconSize = 18;
 export const dateFormat = "MMM YYYY";
-
-
-/**{
-    "name": "Mobile Engineer",
-    "company": "Komodo Inc",
-    "website": "https://komodoplatform.com/en/",
-    "description": "Part of a global team creating dex for crypto and maintaining the alpha version of the software using mainly Dart, Java and using Agile/SCRUM as methodology on Github.",
-    "start": "2022-04-01T00:00:00Z",
-    "end": "2022-08-31T00:00:00Z",
-    "technologies": [
-        "mm2-libs", "flutter-sdks","crypto",
-        "android",
-        "ios","java",
-        "flutter",
-        "dart"
-    ],
-    "icon": "kmd"
-},**/
