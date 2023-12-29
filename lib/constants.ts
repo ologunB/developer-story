fe232ed6af3e5673f81074dc78bb416d601ac2b8
Temitope Daniel <31567966+ologunB@users.noreply.github.com>
2023-12-29 15:08:34 +0100

    update size

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,5 +1,5 @@
 export const maxExpandedLimit = 5;
 export const avatarImageSize = 64;
 export const itemImageSize = 48;
-export const iconSize = 18;
+export const iconSize = 24;
 export const dateFormat = "MMM YYYY";
